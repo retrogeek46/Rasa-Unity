d386b607f6107cd861dc230a984da6fbcfd0e7df
RetroGeek46 <divyangpradeep@gmail.com>
2020-02-10 17:30:09 +0530

    Implement Rasa and Unity connection
    Rasa and Unity communicate to each other through web requests. Hence
    first create a web server/hook over at Rasa side, done through
    custom_channel.py. Then include/add reference to that through
    credentials.yml. This concludes work on Rasa side
    
    Over at Unity, create a script that can create post requests (Use
    UnityWebRequest library from UnityEngine.Networking) and hit the url
    (http://server.ip:5005/webhooks/customIO/webhook). The user message
    needs to be wrapped in a json object which can be created by
    encapsulating necessary keys (sender, message) into a class/struct and
    then using Unity's JsonUtility.ToJson() function.

---
+++ Unity/Rasa/Assets/__Scripts/NetworkManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public struct PostMessage {
+    public string message;
+    public string sender;
+}
+
+
+public class NetworkManager : MonoBehaviour {
+
+    public InputField inputBox;
+    public Text display;
+    private const string rasa_url = "http://127.0.0.1:5005/webhooks/unity/webhook";
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    public void SendMessage () {
+        // Get message from textbox and clear the input field
+        string message = inputBox.text;
+        inputBox.text = "";
+        print("User entered : " + message);
+
+        // Create a json object from user message
+        PostMessage postMessage = new PostMessage {
+            sender = "doku",
+            message = message
+        };
+
+        string jsonBody = JsonUtility.ToJson(postMessage);
+        print("User json : " + jsonBody);
+
+        // Create a post request with the data to send to Rasa server
+        StartCoroutine(PostRequest(rasa_url, jsonBody));
+    }
+
+    private IEnumerator PostRequest (string url, string jsonBody) {
+        UnityWebRequest request = new UnityWebRequest(url, "POST");
+        byte[] rawBody = new System.Text.UTF8Encoding().GetBytes(jsonBody);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(rawBody);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        Debug.Log("Response: " + request.downloadHandler.text);
+    }
+}
